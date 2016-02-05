fe2d7edafc6d29d2d0b01faac9e3c1caf9c8611b
Aleksandr Elesin <ajelesin@gmail.com>
2016-02-05 21:06:02 +0500

    init commit

---
+++ test1/ClassInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace test1
+{
+    public class ClassInfo
+    {
+        public TypeInfo Class { get; set; }
+        public MethodInfo[] Methods { get; set; }
+
+        public override string ToString()
+        {
+            var str = new StringBuilder();
+
+            if (Class != null)
+            {
+                str.AppendLine($"- {Class.FullName}");
+            }
+
+            Methods?
+                .Select(o => $"  - {Class?.FullName}.{o?.Name}")
+                .ForEach(o => str.AppendLine(o));
+
+            return str.ToString();
+        }
+    }
+}
---
+++ test1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace test1
+{
+    class Program
+    {
+        public IEnumerable<ClassInfo> ExplorerFolder(DirectoryInfo directory)
+        {
+            var files = directory.GetFiles("*.dll");
+            var types = files.SelectMany(GetTypes);
+            var methods = types.Select(GetPublicAndProtectedMethods);
+
+            return methods;
+        }
+
+        private IEnumerable<TypeInfo> GetTypes(FileInfo file)
+        {
+            var assembly = Assembly.LoadFile(file.FullName);
+            var types = assembly.GetTypes();
+            var typesInfo = types.Select(type => type.GetTypeInfo());
+            return typesInfo;
+        }
+
+        private ClassInfo GetPublicAndProtectedMethods(TypeInfo type)
+        {
+            var methods = type.GetMethods(BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                .Where(o => o.IsFamily || o.IsPublic);
+
+            var methodInfos = methods.ToArray();
+            return new ClassInfo {Class = type, Methods = methodInfos};
+        }
+
+
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var dirName = Console.ReadLine();
+                var dirInfo = new DirectoryInfo(dirName);
+
+                var program = new Program();
+                var classInfo = program.ExplorerFolder(dirInfo);
+
+                classInfo.ForEach(Console.WriteLine);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex);
+            }
+
+            // просто чтобы консолька не закрывалась
+            Console.ReadLine();
+        }
+    }
+}
